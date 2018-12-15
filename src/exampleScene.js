0f6b0440508ef4d58cbbdb7d323f00addb40476e
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2018-12-15 14:31:51 +1100

    Loading css

--- src/exampleScene.js
+++ src/exampleScene.js
@@ -4,14 +4,14 @@ import Icon from './icon.png';
 export default class ExampleScene {
 
     constructor() {
-    
+        
         let scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+        
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
-
+        
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var texture = new THREE.TextureLoader().load( Icon );
         var material = new THREE.MeshBasicMaterial( { map: texture } );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import ExampleScene from "./ExampleScene";
 import './style.css';
+import Icon from './icon.png';
 
-let exampleScene = new ExampleScene();
+function component() {
+    var element = document.createElement('div');
+    // element.classList.add('hello');
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    let exampleScene = new ExampleScene();
+    return element;
+  }
 
+  document.body.appendChild(component());
 
 
