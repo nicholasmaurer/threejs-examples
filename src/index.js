d2e01f46ff7ade77fd29d708ea9b01e1651574a3
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2018-12-16 14:53:16 +1100

    Output Management and Hot Reloading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import ExampleScene from "./ExampleScene";
 import './style.css';
 import Icon from './icon.png';
+import _ from 'lodash';
+import printMe from "./print";
 
 function component() {
+    
+  let exampleScene = new ExampleScene();
     var element = document.createElement('div');
-    // element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
     // Add the image to our existing div.
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
-    let exampleScene = new ExampleScene();
+    
+    
+    var btn = document.createElement('div');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    
     return element;
   }
 
