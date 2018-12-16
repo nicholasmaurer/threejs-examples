c39323fb1f530fa39887c8acc3d57ab2fd8ef464
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2018-12-16 14:53:52 +1100

    Output Management and HotReloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+  contentBase: './dist'
+  },
    plugins: [
+     new CleanWebpackPlugin(['dist']),
      new HtmlWebpackPlugin({
        title: 'Output Management'
      })
